0d8a8fd0c1740c610e14b60278e56392b9d7b311
Kare Andersen <kareandersen@gmail.com>
2015-10-14 09:13:28 +0100

    nice rotator, bad sine

--- atari/calc.asm
+++ atari/calc.asm
@@ -8,6 +8,16 @@ whost		MACRO	src
 		movep	src,X:<<$ffeb
 		ENDM
 
+mshr		MACRO	s,m,n,acc
+		move	#@pow(2,-n),m
+		mpy	s,m,acc
+		ENDM
+
+mshl		MACRO	s,m,n,acc
+		move	#>@cvi(@pow(2,n-1)),m
+		mpy	s,m,acc
+		ENDM
+
 		ORG	P:$0000
 		jmp	>start
 
@@ -16,12 +26,44 @@ whost		MACRO	src
 start:
                 ori     #04,OMR     ;enable data roms
 
-                rhost	a		;a : t
-		;move	#>128,a
+                rhost	a		;a : t		
 		move	#>255,x0
+		move	a,b
 		move	#time,r0
 		and	x0,a
 		move	a,p:(r0)
+		;move	#>3,x0
+		;nop
+		;and	x0,b
+		;rep	#3
+		;lsr	a
+		;nop
+		;move	a,x1
+
+		move	#1,x0
+		move    #>$100,r1	;r1 : sin
+                move    #>$ff,m1	;m1 : sin wraparound
+		move	a,n1		;n1 : t wrapped
+		nop
+		;move    y:(r1+n1),b
+		;nop
+		;add	x0,a
+		;nop
+		;move	a,n1
+		;nop
+		;move    y:(r1+n1),a
+		;nop
+		;move	a,x0
+		;sub	a,b
+		;nop
+		;move	b,y0
+		;mpy	x1,y0,b
+		;nop
+		;add	y0,b
+		;nop
+		;move	b,a
+
+
 
 		move	#rowU,r2
 		move	#rowV,r3
@@ -30,7 +72,7 @@ start:
 		move    #duRow,r6
 		move	#dvRow,r7
 
-		move	#>128,x0	;centre u and v
+		move	#>32,x0	;centre u and v
 		nop
 		move	x0,p:(r2)	;store centred u
 		move	x0,p:(r3)	;store centred v
@@ -40,18 +82,23 @@ start:
 		move	a,n1		;n1 : t wrapped
 		nop
 		move    y:(r1+n1),y0	;sin(t) for duCol (and dvRow)
-		move	#>1<<(3-1),y1	;the factor 256
+;		move	#>1<<(3-1),y1	;scale factor
+		move	#>6,y1
 		nop
 		mpy	y1,y0,b		;scale duCol to the range [-128..127]
 		move	#>128,x1
 		move	b,a
 		nop
 		add	x1,b
-
+		nop
 		move    b,p:(r4)	;store duCol
+
 		neg	a		;dvRow = -duCol
 		nop
 		add	x1,a
+		nop
+		rep	#8
+		lsl	a
 		nop
 		move	a,p:(r7)	;store dvRow
 
@@ -62,16 +109,19 @@ start:
                 move    #>$ff,m1	;m1 : sin wraparound
 		nop
 		move	y:(r1+n1),y0	;y1: cos(t)
-		move	#>1<<(3-1),y1	;the factor 256
+		;move	#>1<<(3-1),y1	;scale factor
+		move	#>3,y1
 		nop
 		mpy	y1,y0,b		;scale dvCol=dvRow to the range [-128..127]
 		move	#>128,x1
 		nop
 		add	x1,b
 		nop
-
-		move	b,p:(r5)	;store dvCol = cos(t)
 		move	b,p:(r6)	;store duRow = cos(t)
+		nop
+		rep	#8
+		lsl	b
+		move	b,p:(r5)	;store dvCol = cos(t)
 		nop
 
 _start_line
@@ -79,31 +129,19 @@ _start_line
 
 		move	p:(r2),a	;a : u = rowU
 		move	p:(r3),b	;b : v = rowV
-		nop
-
-		do      #160,_end_line
-
-		move	a,x1
-		move	b,y1
-
-		rep	#8
-		asl	b
-		nop
-		add	b,a
-		nop
-                whost	a
-		nop
-		move	x1,a
-		move	y1,b
 
 		move	p:(r6),x0	;x0 : duRow
 		move	p:(r7),y0	;y0 : dvRow
-		move	#>$ff,x1
-		add	x0,a
-		add	y0,b
-		and	x1,a
-		and	x1,b
 
+		do      #160,_end_line
+
+		move	b,r1
+		add	a,b
+                whost	b
+		move	r1,b
+
+		add	x0,a
+		add	y0,a
 
 _end_line
 		move	p:(r2),a	;a : u = rowU
@@ -111,7 +149,7 @@ _end_line
 		move	#>$ff,x1
 
 		move	p:(r4),x0	;x0 : duCol
-		move	p:(r5),y0	;y0 : dvCol		
+		move	p:(r5),y0	;y0 : dvCol
 
 		add	x0,a
 		add	y0,b
